7b8092a2897b83343d84a95f0312fadb5960c8d8
88rabbit <ljj88tu1988@gmai.com>
2015-08-15 17:30:28 -0500

    add test

---
+++ test/TestCalculator.java
@@ -0,0 +1,24 @@
+package andycpp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCalculator {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() 
+	{
+		Calculator cal = new Calculator();
+		int result = cal.add(1, 2);
+		
+		assertTrue(result==3);
+		
+	}
+
+}
