3614be1bc168006b6be4688fe88cf3f66e4fdac7
88rabbit <ljj88tu1988@gmai.com>
2015-08-16 18:31:04 -0500

    test

--- src/andycpp/TestCalculator.java
+++ src/andycpp/TestCalculator.java
@@ -2,14 +2,9 @@ package andycpp;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class TestCalculator {
-
-	@Before
-	public void setUp() throws Exception {
-	}
 
 	@Test
 	public void test() 
